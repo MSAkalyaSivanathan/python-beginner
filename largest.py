45f5bf0f75556beba50ef6a16a16a56744afe6a2
MSAkalyaSivanathan <akaldevi9@gmail.com>
2024-06-13 13:44:26 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,24 @@
+list1=[10,20,4]
+l=max(list1)
+print("largest number:",l)
+num=[10,20,30,40,50,60]
+print("largest number:",max(num))
+x=2
+y=5
+z=10
+print(max(x,y,z))
+num1=10
+num2=20
+num3=30
+if(num1>=num2)and(num1>=num3):
+    l=num1
+elif(num2>=num3)and(num2>=num1):
+    l=num2
+else:
+    l=num3
+print("largest number:",l)
+lst=[30,99,1,49,20,44]
+lst.sort()
+print(lst[-1])
+
+ 
